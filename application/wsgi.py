22295a41c9512045baff1bb0fc1b0d37271f164a
Serge <antipov.serge@gmail.com>
2013-05-23 01:40:42 +0400

    wsgi.py

--- application/wsgi.py
+++ application/wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# import sys
-# import os
-# sys.path.insert(0, os.path.dirname(__file__))
-# sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from app import app as application
