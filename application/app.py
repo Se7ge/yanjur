f478a73b64014afc27a6c0d6a5ac06cbc6951193
Serge <antipov.serge@gmail.com>
2013-05-25 13:45:14 +0400

    Teardrop

--- application/app.py
+++ application/app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 from application.views import *
 
 
+@app.teardown_request
+def shutdown_session(exception=None):
+    Session.remove()
+
+
 if __name__ == '__main__':
     app.run()
