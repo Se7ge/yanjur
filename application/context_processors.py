9b9b93681c0d48ce0d817946526dd785fec0577e
Serge <antipov.serge@gmail.com>
2013-06-14 02:11:24 +0400

    Dark menu

--- application/context_processors.py
+++ application/context_processors.py
@@ -9,12 +9,12 @@ session = Session()
 
 @app.context_processor
 def sidebar_menu():
-    works = session.query(Work).order_by('number').count()
-    persons = session.query(Person).order_by('name').count()
-    titles = session.query(Title).order_by('name').count()
-    actions = session.query(Action).order_by('name').count()
-    places = session.query(Place).order_by('name').count()
-    times = session.query(Work_Time).order_by('name').count()
+    works = session.query(Work).count()
+    persons = session.query(Person).count()
+    titles = session.query(Title).count()
+    actions = session.query(Action).count()
+    places = session.query(Place).count()
+    times = session.query(Work_Time).count()
     return dict(works=works,
                 persons=persons,
                 titles=titles,
