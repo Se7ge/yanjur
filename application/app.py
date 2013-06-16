93cb36cfefaa0d2e47ec9bdb99370e65b9014d96
Serge <antipov.serge@gmail.com>
2013-06-16 19:04:38 +0400

    Translation

--- application/app.py
+++ application/app.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, g
+from flask.ext.babelex import Babel
 from settings import DEBUG
 from admin.database import shutdown_session, Session
 
 app = Flask(__name__)
 app.debug = DEBUG
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    # # if a user is logged in, use the locale from the user settings
+    # user = getattr(g, 'user', None)
+    # if user is not None:
+    #     return user.locale
+    # otherwise try to guess the language from the user accept
+    # header the browser transmits.  We support de/fr/en in this
+    # example.  The best match wins.
+    return request.accept_languages.best_match(['ru_RU', 'en_EN'])
 
 from application.views import *
 
